32ce85ad48b9e03dca5450d62a2bd7e6495c86f6
Jasson Qasqant <jcascante@gmail.com>
2012-12-13 16:07:16 -0600

    Update js/cookieHandler.js
    Value not needed when deleting, duh!

--- js/cookieHandler.js
+++ js/cookieHandler.js
@@ -21,8 +21,7 @@ var cookieHandler = {
   },
   trash: function(name, path, domain) {
     if(this.get(name)) {
-      var value = this.get(name);
-      document.cookie = name + '='+value+'; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/';
+      document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/';
     }
   }
 };
